56b57015237748be50b637320fe17cca544f44b9
roman <roman.cheburan@gmail.com>
2022-05-02 12:51:51 +0300

    initial

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
     ).register(app, path="/webhook")
     setup_application(app, dispatcher, bot=bot)
 
-    run_app(app, host="0.0.0.0", port=8000)
+    run_app(app, host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
